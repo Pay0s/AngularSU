f916156ab796ba63074eb39fbbdaf9cd3fb524ea
payos <paulannes.pro@gmail.com>
2021-06-29 13:54:10 +0200

    Exercice 3

--- src/app/models/statistique/statistique.component.ts
+++ src/app/models/statistique/statistique.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { stat } from 'fs';
+import { statistique } from '../Statistique';
 
 @Component({
   selector: 'app-statistique',
@@ -8,7 +10,8 @@ import { Component, OnInit } from '@angular/core';
 export class StatistiqueComponent implements OnInit {
 
 
-
+Ferrari = new statistique("Ferrari", 560, 22)
+Corvette = new statistique("Corvette",460, 12)
 
 
   constructor() { }
